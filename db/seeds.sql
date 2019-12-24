97ea0fe9b7c2ff889ef9ddf434e52c725809cb5a
Zachary Nylen <znylen@aol.com>
2019-12-23 20:37:03 -0800

    changes

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Turkey Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", true);
